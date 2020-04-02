3830edbd7902c6fb6114f5f1141c107196604401
jurkko <jure.turk@student.um.si>
2020-04-02 19:44:41 +0200

    minor changes

--- message_studio/resources/views/MainSite/about.blade.php
+++ message_studio/resources/views/MainSite/about.blade.php
@@ -118,14 +118,14 @@
 								
 							<div class="col-md-8 col-md-offset-4 text-center fh5co-heading">
 							<h1>Ksenija Lovrenčič</h1>
-							<img class="img-responsive" src="images/ksenija_krog.png" alt="about"><br/>
+							<img class="img-responsive" src="images/ksenija_krog.png" alt="about">
 									<div class="desc">
-										<p class="o_meni">Sem terapevtska maserka, vedno nasmejana in pozitivno usmerjena. Zanima me vse, kar je povezano
+											<h4>Sem terapevtska maserka, vedno nasmejana in pozitivno usmerjena. Zanima me vse, kar je povezano
 											 s telesom in zdravim načinom življenja, zato že kar nekaj let sama izdelujem domačo kozmetiko, za
 											 lastno porabo. Prepričana sem, da lahko sami veliko pripomoremo k izboljšanju zdravja in kvalitete
-											 našega življenja.</p> 
-										<p class="o_meni">Masaža je velik del mojega življenja. Morda se kot mlajša punca tega nisem zavedala, vendar je življenje poskrbelo, da me usmeri na tisto pot, ki je prava zame. Svoje znanje rada nadgrajujem in izpopolnjujem. V veliko zadovoljstvo mi je, da s svojim znanjem in izkušnjami pomagam ljudem, zaradi vsakodnevnih naporov, obremenitev in stresa, ter vam tako pomagam ponovno vzpostaviti energetsko ravnovesje in izboljšati fizično stanje telesa.</p>
-										<p class="quotes">“Zdravje je največje darilo, zadovoljstvo je največje bogastvo, zvestoba najboljši odnos.” - Buddha</p>
+											 našega življenja.</h4>
+											<h4>Masaža je velik del mojega življenja. Morda se kot mlajša punca tega nisem zavedala, vendar je življenje poskrbelo, da me usmeri na tisto pot, ki je prava zame. Svoje znanje rada nadgrajujem in izpopolnjujem. V veliko zadovoljstvo mi je, da s svojim znanjem in izkušnjami pomagam ljudem, zaradi vsakodnevnih naporov, obremenitev in stresa, ter vam tako pomagam ponovno vzpostaviti energetsko ravnovesje in izboljšati fizično stanje telesa.</h4>
+											<h4 class="quotes">“Zdravje je največje darilo, zadovoljstvo je največje bogastvo, zvestoba najboljši odnos.” - Buddha</h4></p>
 									</div>
 							</div>
 							
