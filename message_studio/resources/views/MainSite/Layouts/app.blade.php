12ed3ba58c08a81abf03b57d69d10db3bc5ec7f2
jurkko <jure.turk@student.um.si>
2020-04-21 06:43:24 +0200

    check if layout is working on heroku...

--- message_studio/resources/views/MainSite/Layouts/app.blade.php
+++ message_studio/resources/views/MainSite/Layouts/app.blade.php
@@ -60,19 +60,20 @@
 </head>
     <body>
 
+    @include('cookieConsent::index')
         <div id="page">
-            <nav class="fh5co-nav" role="navigation">
+        <nav class="fh5co-nav" role="navigation">
                 <div class="container">
-                    <div class="row">
-                        <div class="col-xs-2">
-                            <div id="fh5co-logo"><a href="index.html">Pallas</a></div>
+                    <div class="row navbar">
+						<div class="col-xs-2 logo">
+                            <a href="{{ route('RouteController.home')}}"><img class="img-responsive" src="images/logos/lotus_mini_logoo.png" alt="lotus logo"></a>
                         </div>
                         <div class="col-xs-10 menu-1 text-right">
                             <ul>
-                                <li id=""><a href="{{URL::to('/')}}">Domov</a></li>
-                                <li id="omeni"><a href="{{URL::to('/omeni')}}">O meni</a></li>
-                                <li id="ponudba" class="has-dropdown">
-                                    <a href="{{ route('RouteController.ponudba')}}">Ponudba</a>
+								<li><a href="{{ route('RouteController.home')}}">Domov</a></li>
+                                <li><a href="{{ route('RouteController.about')}}">O meni</a></li>
+                                <li class="has-dropdown">
+                                    <a href="{{ route('RouteController.offer')}}">Ponudba</a>
                                     <ul class="dropdown">
                                         <li><a href="#">Klasična masaža</a></li>
                                         <li><a href="#">Športna masaža</a></li>
@@ -82,16 +83,15 @@
                                     </ul>
                                 </li>
                             
-                                <li id="cenik"><a href="{{ route('RouteController.cenik')}}">Cenik</a></li>
-                                <li id="galerija"><a href="{{ route('RouteController.galerija')}}">Galerija</a></li>
-                                <li id="blog"><a href="{{ route('RouteController.blog')}}">Blog</a></li>
-                                <li id="povprasevanje"><a href="{{ route('RouteController.povprasevanje')}}">Povpraševanje</a></li>
+                                <li><a href="{{ route('RouteController.pricelist')}}">Cenik</a></li>
+                                <li><a href="{{ route('RouteController.gallery')}}">Galerija</a></li>
+                                <li><a href="{{ route('RouteController.blog')}}">Blog</a></li>
+                                <li  class="active"><a href="{{ route('RouteController.contact')}}">Kontakt</a></li>
                             </ul>
                         </div>
                     </div>
                 </div>
             </nav>
-
         
 
             @yield('cover')
